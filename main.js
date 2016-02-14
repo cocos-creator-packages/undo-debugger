b69b1b74d394a4148f876be7a849b1f7c9cfb903
Johnny Wu <KarasAya@gmail.com>
2016-02-14 17:57:09 +0800

    update main process registry

--- main.js
+++ main.js
@@ -20,63 +20,64 @@ module.exports = {
   unload () {
   },
 
-  'undo-debugger:open' () {
-    Editor.Panel.open('undo-debugger.panel');
+  messages: {
+    open () {
+      Editor.Panel.open('undo-debugger.panel');
+    },
+
+    query ( reply ) {
+      let undo = Editor.Undo._global;
+      let result = {
+        position: undo._position,
+        savePosition: undo._savePosition,
+        groups: undo._groups.map(group => {
+          return _dumpGroup(group);
+        }),
+      };
+
+      reply( result );
+    },
+
+    changed ( type ) {
+      let undo = Editor.Undo._global;
+
+      if ( type === 'commit' ) {
+        Editor.sendToPanel(
+          'undo-debugger.panel',
+          'undo-debugger:add-group', {
+            position: undo._position,
+            savePosition: undo._savePosition,
+            group: _dumpGroup(undo._groups[undo._position])
+          }
+        );
+      } else if ( type === 'undo' || type === 'redo' ) {
+        Editor.sendToPanel(
+          'undo-debugger.panel',
+          'undo-debugger:position-changed', {
+            position: undo._position,
+          }
+        );
+      } else if ( type === 'save' ) {
+        Editor.sendToPanel(
+          'undo-debugger.panel',
+          'undo-debugger:save-position-changed', {
+            savePosition: undo._savePosition,
+          }
+        );
+      } else if ( type === 'clear-redo' ) {
+        Editor.sendToPanel(
+          'undo-debugger.panel',
+          'undo-debugger:clear-redo', {
+            position: undo._position,
+            savePosition: undo._savePosition,
+          }
+        );
+      } else if ( type === 'clear' ) {
+        Editor.sendToPanel(
+          'undo-debugger.panel',
+          'undo-debugger:clear'
+        );
+      }
+    },
   },
-
-  'undo-debugger:query' ( reply ) {
-    let undo = Editor.Undo._global;
-    let result = {
-      position: undo._position,
-      savePosition: undo._savePosition,
-      groups: undo._groups.map(group => {
-        return _dumpGroup(group);
-      }),
-    };
-
-    reply( result );
-  },
-
-  'undo:changed' ( type ) {
-    let undo = Editor.Undo._global;
-
-    if ( type === 'commit' ) {
-      Editor.sendToPanel(
-        'undo-debugger.panel',
-        'undo-debugger:add-group', {
-          position: undo._position,
-          savePosition: undo._savePosition,
-          group: _dumpGroup(undo._groups[undo._position])
-        }
-      );
-    } else if ( type === 'undo' || type === 'redo' ) {
-      Editor.sendToPanel(
-        'undo-debugger.panel',
-        'undo-debugger:position-changed', {
-          position: undo._position,
-        }
-      );
-    } else if ( type === 'save' ) {
-      Editor.sendToPanel(
-        'undo-debugger.panel',
-        'undo-debugger:save-position-changed', {
-          savePosition: undo._savePosition,
-        }
-      );
-    } else if ( type === 'clear-redo' ) {
-      Editor.sendToPanel(
-        'undo-debugger.panel',
-        'undo-debugger:clear-redo', {
-          position: undo._position,
-          savePosition: undo._savePosition,
-        }
-      );
-    } else if ( type === 'clear' ) {
-      Editor.sendToPanel(
-        'undo-debugger.panel',
-        'undo-debugger:clear'
-      );
-    }
-  },
-
 };
